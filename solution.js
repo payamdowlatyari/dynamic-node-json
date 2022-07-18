da6d21af7c06c9377a2bf54658286f33ae458a0c
Payam Dowlatyari <pdowlatyari@gmail.com>
2022-07-17 18:31:43 -0700

    add footer nav mode

--- solution.js
+++ solution.js
@@ -1,6 +1,5 @@
 const baseURI  =  "https://dynamic-node-json.herokuapp.com/api";
-// const development = 'http://localhost:8080/api';
-// const baseURI = (process.env.NODE_ENV ? production : development);
+// const baseURI = 'http://localhost:8080/api';
 
 const fetchQuestions = async() => {
 
@@ -14,15 +13,18 @@ const fetchAppendQuestions = async() => {
 
     const questions = await fetchQuestions();
     const container = document.getElementById('container');
+    container.before(createNavbar());
     container.append(createRow(questions));  
+   
+    container.append(createFooter());
 }
 
 const showCodeAnswer = (element) => {
 
     fetch(element.code)
         .then(response => response.text())
-        .then(text => toggleText = text);     
-
+        .then(text => toggleText = text);   
+          
     let toggleText = element.text;
 
     const span = document.createElement('span');
@@ -30,25 +32,58 @@ const showCodeAnswer = (element) => {
 
     const pre = document.createElement('pre');
     pre.classList.add('line-number');
-
+    
     const code = document.createElement('code');
     code.classList.add('language-js');
 
-    pre.addEventListener('click', () => {
+   
 
+    const btn = closeBtn();
+    pre.append(code);
+   
+    btn.addEventListener('click', () => {
             pre.classList.toggle('clicked');
 
-            if (span.textContent == element.text) 
-            span.textContent = toggleText;
-
+            if (span.textContent == element.text) {
+                span.textContent = toggleText;
+                span.append(copyText(span.textContent));
+            }
+                      
             else 
-            span.textContent = element.text;;
+            span.textContent = element.text;   
+  
     }) 
-
+    pre.append(btn);
     code.append(span); 
-    pre.append(code);
+    
 
     return pre;
+}
+
+const copyText = (code) => {
+    const copyBtn = document.createElement('button');
+    copyBtn.innerHTML = '<i class="bi bi-stickies"></i>';
+    copyBtn.classList.toggle('sm-btn');
+
+    copyBtn.addEventListener('click', () => {
+        navigator.clipboard.writeText(code);
+        copyBtn.innerHTML = '<i class="bi bi-stickies-fill"></i>';
+    });
+    return copyBtn;
+}
+
+const closeBtn = () => {
+    const btn = document.createElement('button');
+    btn.innerHTML = '<i class="bi bi-chevron-down"></i>';
+    btn.classList.add('sm-btn');
+
+    btn.addEventListener('click', () => {
+        if (btn.innerHTML == '<i class="bi bi-chevron-down"></i>')
+        btn.innerHTML = '<i class="bi bi-chevron-up"></i>';
+        else
+        btn.innerHTML = '<i class="bi bi-chevron-down"></i>';
+    })
+    return btn;
 }
 
 const createGroup = () => {
@@ -88,6 +123,62 @@ const createRow = (questions) => {
     });
 
     return mainDiv;
+}
+
+const mode = () => {
+    const icon = document.createElement('span');
+    icon.classList.add('mode');
+    icon.innerHTML = '<i class="bi bi-sun-fill"></i>';
+
+    let bg =  document.getElementById('container');
+    let h3 = document.getElementsByTagName('h3');
+
+    icon.addEventListener('click', () => {
+        if (icon.innerHTML == '<i class="bi bi-sun-fill"></i>') {
+            icon.innerHTML = '<i class="bi bi-moon-fill"></i>';
+            bg.style.backgroundColor = '#FEF9E7'; 
+            for(let i = 0; i < h3.length; i++) { h3[i].style.color = '#17202A';}
+        }
+        
+        else {
+            icon.innerHTML = '<i class="bi bi-sun-fill"></i>';
+            bg.style.backgroundColor = '#17202A'; 
+            for(let i = 0; i < h3.length; i++) { h3[i].style.color = '#FEF9E7';}
+        }
+       
+    });
+
+    return icon;
+}
+
+const createNavbar = () => {
+    const nav = document.createElement('nav');
+    nav.classList.add('navbar');
+    nav.classList.add('sticky-top');
+    const container = document.createElement('div');
+    container.classList.add('container-fluid');
+    const brand = document.createElement('a');
+    brand.href = 'https://www.payamd.com/';
+    brand.innerHTML = '<i class="bi bi-house-fill"></i>';
+
+    const title = document.createElement('a');
+    title.href = '/';
+    title.classList.add('main-title');
+    title.text = 'Algorithms for Life';
+
+    container.append(brand);
+    container.append(title);
+    container.append(mode());
+    nav.append(container)
+   
+    return nav;
+}
+
+const createFooter = () => {
+    const footer = document.createElement('div');
+    footer.classList.add('footer');
+    footer.innerHTML = 'Designed by <a href="https://www.payamd.com/"> payamd.com </a> © 2022';
+    return footer;
 }
 
 
