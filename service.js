3c5fd72d7cb7637caf9bf35812cfead2c86ed735
Payam Dowlatyari <pdowlatyari@gmail.com>
2023-10-18 13:54:03 -0700

    update CORS url

--- service.js
+++ service.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 app.use(cors());
 
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', true);
